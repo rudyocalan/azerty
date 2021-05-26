4df14e3f17db6674104bc3487ae0bad75e7aecd0
Rudy Öcalan <rudy.ocalan@sage.com>
2021-05-26 11:26:52 +0200

    add search

---
+++ js/search.js
@@ -0,0 +1,64 @@
+function searchAndShowFiles(searchFilter) {
+    var allFilesRequest = getAllFilesFromStore();
+    allFilesRequest.onsuccess = function () {
+        var results = [];
+        this.result.forEach(file => {
+            if (file.fileName.toLowerCase().indexOf(searchFilter.toLowerCase()) > -1) {
+                results.push(file);
+            }
+        });
+        showFileResults(results);
+    };
+}
+
+function showFileResults(files) {
+    var table = document.getElementById("results_table").getElementsByTagName("tbody")[0];
+
+    while (table.children.length > 0) {
+        table.children[0].remove();
+    }
+
+    files.forEach(file => {
+        var fileRow = table.insertRow(-1);
+
+        var fileName = fileRow.insertCell(-1);
+        fileName.innerHTML = file.fileName;
+
+        var fileYear = fileRow.insertCell(-1);
+        fileYear.innerHTML = file.fileYear;
+
+        var fileLevel = fileRow.insertCell(-1);
+        fileLevel.innerHTML = file.fileLevel;
+
+        var fileSemester = fileRow.insertCell(-1);
+        fileSemester.innerHTML = file.fileSemester;
+
+        var fileModule = fileRow.insertCell(-1);
+        fileModule.innerHTML = file.fileModule;
+
+        var fileDownload = fileRow.insertCell(-1);
+        var fileLink = document.createElement("a");
+        fileLink.href = URL.createObjectURL(file.fileObject);
+        fileLink.download = file.fileName;
+        fileLink.innerHTML = FILE_TYPES[file.fileObject.type];
+        fileDownload.appendChild(fileLink);
+
+        var fileUploadDate = fileRow.insertCell(-1);
+        var localDate = new Date(file.fileUploadDate);
+        fileUploadDate.innerHTML = localDate.toLocaleString('fr-FR');
+    });
+}
+
+window.addEventListener('load', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    var searchFilter = urlParams.get('filter');
+
+    if (!searchFilter) {
+        searchFilter = "";
+    }
+
+    var searchFilterSpan = document.getElementById("search_filter");
+    searchFilterSpan.innerHTML = '"' + searchFilter + '"';
+
+    searchAndShowFiles(searchFilter);
+});
